4691b661bc949995f3a8363bbfd2105eb34453fe
Solbeen0531 <netps0201@gmail.com>
2023-06-13 18:30:37 +0900

    le

--- 20230613_swing/src/kh/lclass/swing/TestSwing.java
+++ 20230613_swing/src/kh/lclass/swing/TestSwing.java
@@ -1,8 +1,6 @@
 package kh.lclass.swing;
 
-import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
