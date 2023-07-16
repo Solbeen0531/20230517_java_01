ed73fbdd0a4ba7d1ba4fb19547bb117403a0654f
Solbeen0531 <netps0201@gmail.com>
2023-07-16 22:21:43 +0900

    db 과제 join sub query
    join과 sub query를 활용한 과제

---
+++ sql/20230714_SYS.sql
@@ -0,0 +1,10 @@
+--사용자 잠금상태 해제
+ALTER USER KH ACCOUNT UNLOCK;
+
+--사용자 잠금상태 설정
+ALTER USER KH ACCOUNT LOCK;
+
+
+
+
+
--- sql/task_s/20230712_KH_SELECT.sql
+++ sql/task_s/20230712_KH_SELECT.sql
@@ -174,4 +174,103 @@ SELECT EMP_NAME, REPLACE(EMP_NO,SUBSTR(EMP_NO,8,7),'*******')
 SELECT *
  FROM EMPLOYEE;
 --answer
-SELECT 
+SELECT EMP_NAME,FLOOR(0 - (HIRE_DATE - SYSDATE)) AS "근무일수1", FLOOR(SYSDATE - HIRE_DATE) AS "근무일수2"
+ FROM EMPLOYEE;
+ 
+--KH_SELECT 연습문제
+--연습문제 18번
+--EMPLOYEE테이블에서 사번이 홀수인 직원들의 정보 모두 조회
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT *
+ FROM EMPLOYEE
+ WHERE MOD(EMP_ID, 2) = 1;
+ 
+--KH_SELECT 연습문제
+--연습문제 19번
+--EMPLOYEE테이블에서 근무 년수가 20년 이상인 직원 정보 조회
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT *
+ FROM EMPLOYEE
+ WHERE MONTHS_BETWEEN(SYSDATE, HIRE_DATE) >= 240;
+
+--KH_SELECT 연습문제
+--연습문제 20번
+--20. EMPLOYEE 테이블에서 사원명, 급여 조회 (단, 급여는 '\9,000,000' 형식으로 표시)
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT EMP_NAME, TO_CHAR(SALARY,'L0,000,000')
+ FROM EMPLOYEE;
+ 
+--KH_SELECT 연습문제
+--연습문제 21번
+--21. EMPLOYEE테이블에서 직원 명, 부서코드, 생년월일, 나이(만) 조회
+--(단, 생년월일은 주민번호에서 추출해서 00년 00월 00일로 출력되게 하며
+--나이는 주민번호에서 출력해서 날짜데이터로 변환한 다음 계산)
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT  EMP_NAME, DEPT_CODE,
+        SUBSTR(EMP_NO,1,2)||'년 '||SUBSTR(EMP_NO,3,2)||'월 '||SUBSTR(EMP_NO,5,2)||'일',
+        EXTRACT(YEAR FROM SYSDATE)-(SUBSTR(EMP_NO,1,2)+1900)
+ FROM EMPLOYEE;
+
+--KH_SELECT 연습문제
+--연습문제 22번
+--22. EMPLOYEE테이블에서 부서코드가 D5, D6, D9인 사원만 조회하되 D5면 총무부, D6면 기획부, D9면 영업부로 처리
+--(단, 부서코드 오름차순으로 정렬)
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT  EMP_ID,EMP_NAME,DEPT_CODE,
+        DECODE(DEPT_CODE,'D5','총무부','D6','기획부','D9','영업부')
+ FROM EMPLOYEE
+ WHERE DEPT_CODE IN('D5','D6','D9')
+ ORDER BY DEPT_CODE ASC;
+ 
+--KH_SELECT 연습문제
+--연습문제 23번
+--23. EMPLOYEE테이블에서 사번이 201번인 사원명, 주민번호 앞자리, 주민번호 뒷자리,
+--주민번호 앞자리와 뒷자리의 합 조회
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT EMP_NAME,SUBSTR(EMP_NO,1,6),SUBSTR(EMP_NO,8,7),SUBSTR(EMP_NO,1,6)+SUBSTR(EMP_NO,8,7)
+ FROM EMPLOYEE
+ WHERE EMP_ID = 201;
+
+--KH_SELECT 연습문제
+--연습문제 24번
+--24. EMPLOYEE테이블에서 부서코드가 D5인 직원의 보너스 포함 연봉 합 조회
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT SUM(SALARY + SALARY*NVL(BONUS,0)*12)
+ FROM EMPLOYEE
+ WHERE DEPT_CODE = 'D5';
+ 
+--KH_SELECT 연습문제
+--연습문제 25번
+--25. EMPLOYEE테이블에서 직원들의 입사일로부터 년도만 가지고 각 년도별 입사 인원수 조회
+--전체 직원 수, 2001년, 2002년, 2003년, 2004년
+--test
+SELECT *
+ FROM EMPLOYEE;
+--answer
+SELECT  COUNT(HIRE_DATE) AS "전체직원수",
+        COUNT(CASE WHEN SUBSTR(HIRE_DATE,1,2)=01 THEN 1 END) AS "2001년",
+        COUNT(CASE WHEN SUBSTR(HIRE_DATE,1,2)=02 THEN 1 END) AS "2002년",
+        COUNT(CASE WHEN SUBSTR(HIRE_DATE,1,2)=03 THEN 1 END) AS "2003년",
+        COUNT(CASE WHEN SUBSTR(HIRE_DATE,1,2)=04 THEN 1 END) AS "2004년"
+ FROM EMPLOYEE;
